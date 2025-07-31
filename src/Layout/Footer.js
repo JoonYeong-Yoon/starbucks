affecb11cb08fb4814570c9a5e9b2eade35c1dfd
Yoon Joonyeong <dbswnsdud48@gmail.com>
2025-07-31 14:15:21 +0900

    1차 저장

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,7 +2,87 @@ function Footer() {
   return (
     <>
       <footer>
-        <div></div>
+        <div>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/company">company</a>
+            </li>
+            <li>
+              <a href="/한눈에 보기">한눈에 보기</a>
+            </li>
+            <li>
+              <a href="/스타벅스 사명">스타벅스 사명</a>
+            </li>
+            <li>
+              <a href="/스타벅스 소개">스타벅스 소개</a>
+            </li>
+            <li>
+              <a href="/컴플라이언스">컴플라이언스</a>
+            </li>
+            <li>
+              <a href="/국내 뉴스룸">국내 뉴스룸</a>
+            </li>
+            <li>
+              <a href="/세계의 스타벅스">세계의 스타벅스</a>
+            </li>
+            <li>
+              <a href="/글로벌 뉴스룸">글로벌 뉴스룸</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/corporate-sales">corporate sales</a>
+            </li>
+            <li>
+              <a href="/단체 및 기업 구매 안내">단체 및 기업 구매 안내</a>
+            </li>
+            <li>
+              <a href="/단체 주문 배달 안내">단체 주문 배달 안내</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/partnership">partnership</a>
+            </li>
+            <li>
+              <a href="/신규 입점 제의">신규 입점 제의</a>
+            </li>
+            <li>
+              <a href="/협력 고객사 등록신청">협력 고객사 등록신청</a>
+            </li>
+            <li>
+              <a href="/중개업체 확인">중개업체 확인</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/community">online community</a>
+            </li>
+            <li>
+              <a href="/페이스북">페이스북</a>
+            </li>
+            <li>
+              <a href="/트위터">트위터</a>
+            </li>
+            <li>
+              <a href="/유튜브">유튜브</a>
+            </li>
+            <li>
+              <a href="/인스타그램">인스타그램</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/recruit">recruit</a>
+            </li>
+            <li>
+              <a href="/채용 소개">채용 소개</a>
+            </li>
+            <li>
+              <a href="/채용 지원하기">채용 지원하기</a>
+            </li>
+          </ul>
+        </div>
         <div></div>
       </footer>
     </>
